ab13f021d9d1e6ff520459185e8c826f6092ad22
Daniel Hengeveld <danielwh@gmail.com>
2021-02-12 08:09:47 -0600

    track lengths

--- aaa_lepseq.lua
+++ aaa_lepseq.lua
@@ -26,7 +26,7 @@ g:refresh()
 -- temporarily init to a scale
 notes = {72, 75, 79, 82} -- c eb g bb
 
-notes_track = {
+note_track = {
   steps = {1,1,1,1,1,1,1,1},
   position = 1,
   length = 8
@@ -42,10 +42,14 @@ function on_set_gate_step(position)
 end
 
 function on_set_note_step(position, value)
-  notes_track.steps[position] = value
+  note_track.steps[position] = value
 end
 
 function on_set_gate_multiplier(multiplier)
+end
+
+function on_set_length(track, length)
+  track.length = length
 end
 
 function light_steps()
@@ -53,29 +57,39 @@ function light_steps()
     if step then g:led(i, 1, 11) end
   end
 
-  for i, step in ipairs(notes_track.steps) do
+  for i, step in ipairs(note_track.steps) do
     for note = 1,step do
       g:led(i, 8 - note,5)      
     end
   end
+
+  g:led(gate_track.length, 2, 15)
+  g:led(note_track.length, 8, 15)
 end
 
 g.key = function(x,y,z)
   -- set step
   -- -- x: 1,8 y: 1
+  
   -- set pitch
   -- -- x: 1,8 y: 3,7
   print(x.." "..y.." "..z)
   if (1 <= x) and (x <= 8) and (3 <= y) and (y <= 7) and (z == 1) then on_set_note_step(x, 8-y) end
+  
   -- pattern length
   -- -- x: 1,8 y:2
   -- -- x: 1,8 y:8
+  if (1 <= x) and (x <= 8) and y == 2 then on_set_length(gate_track, x) end
+  if (1 <= x) and (x <= 8) and y == 8 then on_set_length(note_track, x) end
+  
   -- clock multiplier for each sequencer
   -- -- x: 9,12 y:2
   -- -- x: 9,12 y8  
+  
   -- reverse 
   -- -- x: 16 y: 2
   -- -- x: 16 y: 8
+  
   -- triplet?
 end
 
@@ -85,11 +99,11 @@ function step()
   for key, value in ipairs(gate_track.steps) do
     gates = gates..", "..tostring(value)
   end
-  for key, value in ipairs(notes_track.steps) do
+  for key, value in ipairs(note_track.steps) do
     gates = gates..", "..tostring(value)
   end
   print(gates)
-  notes_track.position = (notes_track.position % notes_track.length) + 1
+  note_track.position = (note_track.position % note_track.length) + 1
   gate_track.position = (gate_track.position % gate_track.length) + 1
 
   draw_grid()
@@ -99,8 +113,8 @@ function light_current_step()
   -- gate
   g:led(gate_track.position, 1, 15)
   -- note
-  for note = 1, notes_track.steps[notes_track.position] do
-    g:led(notes_track.position, 8 - note,15)
+  for note = 1, note_track.steps[note_track.position] do
+    g:led(note_track.position, 8 - note,15)
   end
 end
 
